e7c2574b56d6e76f96565a262f68cd07f89b4be8
R3ffoTz <christoffer@gennerud.se>
2026-01-15 10:04:58 +0100

    Create button.py

---
+++ custom_components/zeekr_api/button.py
@@ -0,0 +1,92 @@
+from homeassistant.components.button import ButtonEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from .const import DOMAIN, URL_CONTROL
+import logging
+import asyncio
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    prefix = entry.data.get("name", "Zeekr")
+    
+    async_add_entities([
+        ZeekrRefreshButton(coordinator, prefix),
+        ZeekrTrunkButton(coordinator, prefix),
+        ZeekrTravelUpdateButton(coordinator, prefix, entry.entry_id)
+    ])
+
+class ZeekrTravelUpdateButton(ButtonEntity):
+    def __init__(self, coordinator, prefix, entry_id):
+        self.coordinator = coordinator
+        vin = coordinator.entry.data.get('vin')
+        self._entry_id = entry_id
+        
+        self._attr_translation_key = "update_travel_plan"
+        self._attr_has_entity_name = True
+        self._attr_unique_id = f"{entry_id}_travel_update"
+        self._attr_icon = "mdi:cloud-upload"
+        
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, vin)},
+            "name": prefix,
+            "manufacturer": "Zeekr",
+        }
+
+    async def async_press(self):
+        travel_switch = self.hass.data[DOMAIN].get(f"{self._entry_id}_travel_switch")
+        if travel_switch:
+            await travel_switch._send_plan("start")
+        else:
+            _LOGGER.error("Travel Switch niet gevonden")
+
+class ZeekrRefreshButton(ButtonEntity):
+    def __init__(self, coordinator, prefix):
+        self.coordinator = coordinator
+        vin = coordinator.entry.data.get('vin')
+        
+        self._attr_translation_key = "refresh_data"
+        self._attr_has_entity_name = True
+        self._attr_unique_id = f"{coordinator.entry.entry_id}_refresh_button"
+        self._attr_icon = "mdi:database-refresh"
+        
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, vin)},
+            "name": prefix,
+            "manufacturer": "Zeekr",
+        }
+
+    async def async_press(self):
+        await self.coordinator.async_refresh()
+
+class ZeekrTrunkButton(CoordinatorEntity, ButtonEntity):
+    def __init__(self, coordinator, prefix):
+        super().__init__(coordinator)
+        vin = coordinator.entry.data.get('vin')
+        
+        self._attr_translation_key = "open_trunk"
+        self._attr_has_entity_name = True
+        self._attr_unique_id = f"{coordinator.entry.entry_id}_trunk_button"
+        self._attr_icon = "mdi:car-back"
+        
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, vin)},
+            "name": prefix,
+            "manufacturer": "Zeekr",
+        }
+
+    async def async_press(self):
+        payload = {
+            "command": "start",
+            "serviceId": "RDU",
+            "setting": {
+                "serviceParameters": [
+                    {
+                        "key": "target",
+                        "value": "trunk"
+                    }
+                ]
+            }
+        }
+
+        await self.coordinator.send_command(URL_CONTROL, payload, "Achterklep openen")
