c42c32403f4423d6875ec9ef0c2eacb0ef85d0e6
R3ffoTz <christoffer@gennerud.se>
2026-01-15 10:06:39 +0100

    Create const.py

---
+++ custom_components/zeekr_api/const.py
@@ -0,0 +1,18 @@
+DOMAIN = "zeekr_7x"
+CONF_NAME = "name"
+PLATFORMS = ["sensor", "binary_sensor", "switch", "button", "climate", "cover", "number", "lock", "device_tracker", "time"]
+
+# API Endpoints
+URL_BASE = "https://eu-snc-tsp-api-gw.zeekrlife.com"
+URL_LOGIN = f"{URL_BASE}/ms-user-auth/v1.0/auth/login"
+URL_CONTROL = f"{URL_BASE}/ms-remote-control/v1.0/remoteControl/control"
+URL_STATUS = f"{URL_BASE}/ms-vehicle-status/api/v1.0/vehicle/status/latest?latest=false&target=new"
+URL_SENTRY = f"{URL_BASE}/ms-app-bff/api/v1.0/remoteControl/getVehicleState"
+URL_TRAVEL = f"{URL_BASE}/ms-charge-manage/api/v1.0/charge/getLatestTravelPlan"
+URL_SET_TRAVEL = f"{URL_BASE}/ms-charge-manage/api/v1.0/charge/setTravelPlan"
+URL_CHARGE_CONTROL = f"{URL_BASE}/ms-charge-manage/api/v1.0/charge/control"
+URL_CHARGE_PLAN = f"{URL_BASE}/ms-charge-manage/api/v1.0/charge/getChargingPlan"
+URL_SET_CHARGE_PLAN = f"{URL_BASE}/ms-charge-manage/api/v1.0/charge/setChargingPlan"
+URL_LIST = f"{URL_BASE}/ms-app-bff/api/v4.0/veh/vehicle-list?needSharedCar=true"
+URL_QRVS = f"{URL_BASE}/ms-vehicle-status/api/v1.0/vehicle/status/qrvs"
+URL_SOC = f"{URL_BASE}/ms-charge-manage/api/v1.0/charge/getLatestSoc"
